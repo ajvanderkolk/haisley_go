624af8484bae37138d3e1750954f177991aa2304
Andrew VanDerKolk <andrewjvanderkolk@gmail.com>
2024-08-12 20:09:28 -0500

    scrape and export to csv.

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module Users/AJV03/haisley_go
 
 go 1.22.6
+
+require github.com/PuerkitoBio/goquery v1.9.2
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.28.0 // indirect
+)
--- scrape.go
+++ scrape.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"io"
+	"github.com/PuerkitoBio/goquery"
+	//"io"
 	"log"
 	"net/http"
+	"os"
 )
+
+// custom type to keep of the target object to scrape
+type Product struct {
+	name, price string
+}
 
 func main() {
 	// download the target HTML document
@@ -20,15 +28,56 @@ func main() {
 		log.Fatalf("HTTP Error %d: %s", response.StatusCode, response.Status)
 	}
 
-	// convert the response buffer to bytes
-	byteBody, err := io.ReadAll(response.Body)
+	// parse the HTML document
+	doc, err := goquery.NewDocumentFromReader(response.Body)
 	if err != nil {
-		log.Fatal("Error while reading the response buffer", err)
+		log.Fatal("Failed to parse the HTML document", err)
 	}
 
-	// convert the byte HTML content to string and
-	// print it
-	html := string(byteBody)
-	fmt.Println(html)
+	// where to store the scraped data
+	var products []Product
 
+	// retrieve name and price from each product and print it
+	doc.Find("li.product").Each(func(i int, p *goquery.Selection) {
+		// scraping logic
+		product := Product{}
+		product.name = p.Find("h2").Text()
+		product.price = p.Find("span.price").Text()
+
+		// store the scraped product
+		products = append(products, product)
+	})
+	fmt.Println(products)
+
+	// print the scraped data
+	// initialize the output CSV file
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatal("Failed to create the output CSV file", err)
+	}
+	defer file.Close()
+
+	// initialize a file writer
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// define the CSV headers
+	headers := []string{
+		"image",
+		"price",
+	}
+	// write the column headers
+	writer.Write(headers)
+
+	// add each product to the CSV file
+	for _, product := range products {
+		// convert a Product to an array of strings
+		record := []string{
+			product.name,
+			product.price,
+		}
+
+		// write a new CSV record
+		writer.Write(record)
+	}
 }
