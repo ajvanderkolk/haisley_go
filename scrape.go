8f82d0670ccee78227431e6c55b67053b18efedc
Andrew VanDerKolk <andrewjvanderkolk@gmail.com>
2024-08-16 20:26:48 -0500

    updated webscraper methods.

--- scrape.go
+++ scrape.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/chromedp"
@@ -90,7 +91,7 @@ func new() {
 func main() {
 	// setup options
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false),
+		chromedp.Flag("headless", true),
 		chromedp.DisableGPU,
 		chromedp.NoSandbox,
 	)
@@ -114,7 +115,7 @@ func main() {
 
 	// ## DO WORK ##
 	// The Navigate action already waits until a page loads.
-	err := chromedp.Run(taskCtx, chromedp.Navigate(`https://www.dibbs.bsm.dla.mil/Awards/`))
+	err := chromedp.Run(taskCtx, chromedp.Navigate("https://www.dibbs.bsm.dla.mil/Awards/"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -122,42 +123,132 @@ func main() {
 	// However, actions like Click don't always trigger a page navigation,
 	// so they don't wait for a page load directly. Wrapping them with
 	// RunResponse does that waiting, and also obtains the HTTP response.
-	resp, err := chromedp.RunResponse(taskCtx, chromedp.Click(`butAgree`, chromedp.NodeVisible, chromedp.ByID))
+	resp, err := chromedp.RunResponse(taskCtx, chromedp.Click("butAgree", chromedp.NodeVisible, chromedp.ByID))
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("consent page status code:", resp.Status)
 
-	// get page 1 table
-	var html string
-	var html2 string
 	err = chromedp.Run(taskCtx,
-		chromedp.Navigate(`https://www.dibbs.bsm.dla.mil/Awards/AwdDates.aspx?category=awddt`),
-		chromedp.Navigate(`https://www.dibbs.bsm.dla.mil/Awards/AwdRecs.aspx?Category=awddt&TypeSrch=cq&Value=08-13-2024`),
-		chromedp.InnerHTML(`ctl00_cph1_grdAwardSearch`, &html, chromedp.ByID),
+		chromedp.Navigate("https://www.dibbs.bsm.dla.mil/Awards/AwdDates.aspx?category=awddt"),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	get_awards_page(taskCtx, "08-13-2024")
 
 	// go to page 2
-	resp, err = chromedp.RunResponse(taskCtx, chromedp.Evaluate(`javascript:__doPostBack('ctl00$cph1$grdAwardSearch','Page$2')`, nil))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("page 2 status code:", resp.Status)
+	//  resp, err = chromedp.RunResponse(taskCtx, chromedp.Evaluate(`javascript:__doPostBack('ctl00$cph1$grdAwardSearch','Page$2')`, nil))
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	fmt.Println("page 2 status code:", resp.Status)
+}
 
-	// get page 2 table
-	err = chromedp.Run(taskCtx,
-		chromedp.InnerHTML(`ctl00_cph1_grdAwardSearch`, &html2, chromedp.ByID),
+type Awards_Row struct {
+	row_num                 string
+	award_num               string
+	delv_order_num          string
+	delv_order_cnt          string
+	last_mod_posting_date   string
+	a_cage_code             string
+	total_contract_price    string
+	award_date              string
+	posted_date             string
+	nsn_part_number         string
+	nomenclature            string
+	purchase_request_number string
+	solicitation_number     string
+}
+
+func get_awards_page(browserCtx context.Context, date string) (Awards_Page []Awards_Row) {
+	// get the table data from the html string
+	// implement goquery to get the table data
+	// implement goquery object to csv
+
+	var target_url string
+	var html string
+	var html_reader *strings.Reader
+
+	target_url = "https://www.dibbs.bsm.dla.mil/Awards/AwdRecs.aspx?Category=awddt&TypeSrch=cq&Value=" + date
+
+	// get the html string
+	err := chromedp.Run(browserCtx,
+		chromedp.Navigate(target_url),
+		chromedp.OuterHTML("ctl00_cph1_grdAwardSearch", &html, chromedp.ByID),
 	)
 	if err != nil {
 		log.Fatal(err)
+	} else {
+		fmt.Println("Got the page")
 	}
 
-	// loop through the links with a pool
-	// "javascript:__doPostBack('ctl00$cph1$grdAwardSearch','Page$X')" 1-100 etc.
-	fmt.Println(html[:100])
-	fmt.Println("nexttttttt...")
-	fmt.Println(html2[:100])
+	fmt.Println(html[:1000])
+	//fmt.Println("nexttttttt...")
+
+	// read the html string
+	html_reader = strings.NewReader(html)
+	doc, err := goquery.NewDocumentFromReader(html_reader)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Got the document")
+	}
+
+	// selection of rows then for each row select the cells and store the data
+	doc.Find("tr").Each(func(i int, row *goquery.Selection) {
+		parsed_row := Awards_Row{}
+
+		// selection of cells (td) in the row
+		row = row.Find("td")
+		// first cell in the row, span, text (might have issue with multiple a tags)
+		parsed_row.row_num = row.First().Find("span").Text()
+		// second cell in the row - span - second a - text
+		parsed_row.award_num = row.Next().Find("span").Find("a").Next().Text()
+		// third cell in the row - span - second a - text
+		parsed_row.delv_order_num = row.Next().Find("span").Find("a").Next().Text()
+		// fourth cell in the row - span - text
+		parsed_row.delv_order_cnt = row.Next().Find("span").Text()
+		// fifth cell in the row - span - text
+		parsed_row.last_mod_posting_date = row.Next().Find("span").Text()
+		// sixth cell in the row - span - a - text
+		parsed_row.a_cage_code = row.Next().Find("span").Find("a").Text()
+		// seventh cell in the row - span - text
+		parsed_row.total_contract_price = row.Next().Find("span").Text()
+		// eighth cell in the row - span - text
+		parsed_row.award_date = row.Next().Find("span").Text()
+		// ninth cell in the row - span - text
+		parsed_row.posted_date = row.Next().Find("span").Text()
+		// tenth cell in the row - span - text
+		parsed_row.nsn_part_number = row.Next().Find("span").Text()
+		// eleventh cell in the row - span - text
+		parsed_row.nomenclature = row.Next().Find("span").Text()
+		// twelfth cell in the row - span - text
+		parsed_row.purchase_request_number = row.Next().Find("span").Text()
+		// thirteenth cell in the row - span - text
+		parsed_row.solicitation_number = row.Next().Find("span").Text()
+
+		// store the scraped row
+		Awards_Page = append(Awards_Page, parsed_row)
+	})
+
+	finder := "tbody"
+	fmt.Println("Finder: ", finder)
+
+	sel := doc.Children()
+	for i := range sel.Nodes {
+		single := sel.Eq(i)
+		h, err := single.Html()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Node ", i, " text: ", h[0:5])
+	}
+
+	//fmt.Println("first 100: ", html[:100])
+
+	//fmt.Println("Awards Page object: ", Awards_Page)
+
+	return Awards_Page
 }
