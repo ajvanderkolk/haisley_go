4c710a69108817512e1cabf0b3f4a9f649c183dc
Andrew VanDerKolk <andrewjvanderkolk@gmail.com>
2024-08-17 00:45:27 -0500

    scraper functionality updated. next working on func to scrape all tables for a page.

--- scrape.go
+++ scrape.go
@@ -88,10 +88,145 @@ func new() {
 	}
 }
 
+type Awards_Row struct {
+	row_num                 string
+	award_num               string
+	delv_order_num          string
+	delv_order_cnt          string
+	last_mod_posting_date   string
+	a_cage_code             string
+	total_contract_price    string
+	award_date              string
+	posted_date             string
+	nsn_part_number         string
+	nomenclature            string
+	purchase_request_number string
+	solicitation_number     string
+}
+
+func track(msg string) (string, time.Time) {
+	return msg, time.Now()
+}
+
+func duration(msg string, start time.Time) {
+	fmt.Printf("%v: %v\n", msg, time.Since(start))
+}
+
+func get_awards_page(browserCtx context.Context) (Awards_Page [][]string) {
+	/*
+		get the table data from the html string
+		implement goquery to get the table data
+		implement goquery object to csv
+	*/
+
+	defer duration(track("get_awards_page"))
+
+	var html string
+	var html_reader *strings.Reader
+
+	// get the html string
+	err := chromedp.Run(browserCtx,
+		chromedp.OuterHTML("ctl00_cph1_grdAwardSearch", &html, chromedp.ByID),
+	)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Got the page")
+	}
+
+	//fmt.Println(html[:1000])
+
+	// read the html string
+	html_reader = strings.NewReader(html)
+	doc, err := goquery.NewDocumentFromReader(html_reader)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Got the document")
+	}
+
+	// selection of rows then for each row select the cells and store the data
+	doc.Find("tr.BgWhite, tr.BgSilver").Each(func(i int, row *goquery.Selection) {
+		parsed_row := Awards_Row{}
+
+		// selection of cells (td) in the row
+		row = row.Find("td")
+		// first cell in the row, span, text (might have issue with multiple a tags)
+		parsed_row.row_num = row.Eq(0).Find("span").Text()
+		// second cell in the row - span - second a - text
+		parsed_row.award_num = row.Eq(1).Find("span").Find("a").Next().Text()
+		// third cell in the row - span - second a - text
+		parsed_row.delv_order_num = row.Eq(2).Find("span").Find("a").Next().Text()
+		// fourth cell in the row - span - text
+		parsed_row.delv_order_cnt = row.Eq(3).Find("span").Text()
+		// fifth cell in the row - span - text
+		parsed_row.last_mod_posting_date = row.Eq(4).Find("span").Text()
+		// sixth cell in the row - span - a - text
+		parsed_row.a_cage_code = strings.TrimSpace(row.Eq(5).Find("span").Find("a").Text())
+		// seventh cell in the row - span - text
+		parsed_row.total_contract_price = row.Eq(6).Find("span").Text()
+		// eighth cell in the row - span - text
+		parsed_row.award_date = row.Eq(7).Find("span").Text()
+		// ninth cell in the row - span - text
+		parsed_row.posted_date = row.Eq(8).Find("span").Text()
+		// tenth cell in the row - span - text
+		parsed_row.nsn_part_number = row.Eq(9).Find("span").Text()
+		// eleventh cell in the row - span - text
+		parsed_row.nomenclature = row.Eq(10).Find("span").Text()
+		// twelfth cell in the row - span - text
+		parsed_row.purchase_request_number = row.Eq(11).Find("span").Text()
+		// thirteenth cell in the row - span - text
+		parsed_row.solicitation_number = row.Eq(12).Find("span").Text()
+
+		//fmt.Println("Row: ", parsed_row)
+
+		parsed_row_list := []string{
+			parsed_row.row_num,
+			parsed_row.award_num,
+			parsed_row.delv_order_num,
+			parsed_row.delv_order_cnt,
+			parsed_row.last_mod_posting_date,
+			parsed_row.a_cage_code,
+			parsed_row.total_contract_price,
+			parsed_row.award_date,
+			parsed_row.posted_date,
+			parsed_row.nsn_part_number,
+			parsed_row.nomenclature,
+			parsed_row.purchase_request_number,
+			parsed_row.solicitation_number,
+		}
+		// store the scraped row
+		Awards_Page = append(Awards_Page, parsed_row_list)
+	})
+
+	/* finder := "tr.BgWhite, tr.BgSilver"
+	fmt.Println("Finder: ", finder)
+
+	//sel := doc.Find(finder).Find("td")
+	sel := doc.Find("tr.BgWhite, tr.BgSilver").Find("td").First()
+	for i := range sel.Nodes {
+		single := sel.Eq(i)
+		h, err := single.Html()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Node ", i, " text: ", h)
+	}
+	*/
+
+	return Awards_Page
+}
+
+// TODO: implement the pagination
+// for pagination, run the javascript then call the get_awards_page function
+// and append what returns
+// switch from struct type to list of strings
+// TODO: implement Award_Page to CSV
+
 func main() {
 	// setup options
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", true),
+		chromedp.Flag("headless", false),
 		chromedp.DisableGPU,
 		chromedp.NoSandbox,
 	)
@@ -129,126 +264,40 @@ func main() {
 	}
 	fmt.Println("consent page status code:", resp.Status)
 
-	err = chromedp.Run(taskCtx,
-		chromedp.Navigate("https://www.dibbs.bsm.dla.mil/Awards/AwdDates.aspx?category=awddt"),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	get_awards_page(taskCtx, "08-13-2024")
-
-	// go to page 2
-	//  resp, err = chromedp.RunResponse(taskCtx, chromedp.Evaluate(`javascript:__doPostBack('ctl00$cph1$grdAwardSearch','Page$2')`, nil))
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//	fmt.Println("page 2 status code:", resp.Status)
-}
-
-type Awards_Row struct {
-	row_num                 string
-	award_num               string
-	delv_order_num          string
-	delv_order_cnt          string
-	last_mod_posting_date   string
-	a_cage_code             string
-	total_contract_price    string
-	award_date              string
-	posted_date             string
-	nsn_part_number         string
-	nomenclature            string
-	purchase_request_number string
-	solicitation_number     string
-}
-
-func get_awards_page(browserCtx context.Context, date string) (Awards_Page []Awards_Row) {
-	// get the table data from the html string
-	// implement goquery to get the table data
-	// implement goquery object to csv
-
+	// declare variables
+	var date string
 	var target_url string
-	var html string
-	var html_reader *strings.Reader
-
+	var last_page string
+	date = "08-13-2024"
 	target_url = "https://www.dibbs.bsm.dla.mil/Awards/AwdRecs.aspx?Category=awddt&TypeSrch=cq&Value=" + date
 
-	// get the html string
-	err := chromedp.Run(browserCtx,
+	// navigate to the target url then target date page and get table 1 of records
+	err = chromedp.Run(taskCtx,
+		chromedp.Navigate("https://www.dibbs.bsm.dla.mil/Awards/AwdDates.aspx?category=awddt"),
 		chromedp.Navigate(target_url),
-		chromedp.OuterHTML("ctl00_cph1_grdAwardSearch", &html, chromedp.ByID),
+		chromedp.Evaluate(`javascript:__doPostBack('ctl00$cph1$grdAwardSearch','Page$Last')`, nil),
+		chromedp.InnerHTML(".pagination span", &last_page, chromedp.BySearch),
+		chromedp.Navigate(target_url),
 	)
 	if err != nil {
 		log.Fatal(err)
+		fmt.Println("Error navigating to the target url for ", date, " or table 1 ")
 	} else {
-		fmt.Println("Got the page")
+		fmt.Println("Last page: ", last_page)
+		fmt.Println("Navigated to the first page of ", date)
+		page_1 := get_awards_page(taskCtx)
+		fmt.Println("Page 1: ", page_1[:1])
 	}
 
-	fmt.Println(html[:1000])
-	//fmt.Println("nexttttttt...")
-
-	// read the html string
-	html_reader = strings.NewReader(html)
-	doc, err := goquery.NewDocumentFromReader(html_reader)
-	if err != nil {
+	// get to table 2 of records
+	resp, err = chromedp.RunResponse(taskCtx, chromedp.Evaluate(`javascript:__doPostBack('ctl00$cph1$grdAwardSearch','Page$2')`, nil))
+	if err != nil || resp.Status != 200 {
 		log.Fatal(err)
+		fmt.Println("Error retrieving table 2. status code: ", resp.Status)
 	} else {
-		fmt.Println("Got the document")
+		fmt.Println("Page 2 status code: ", resp.Status)
+		page_2 := get_awards_page(taskCtx)
+		fmt.Println("Page 2: ", page_2[:1])
 	}
 
-	// selection of rows then for each row select the cells and store the data
-	doc.Find("tr").Each(func(i int, row *goquery.Selection) {
-		parsed_row := Awards_Row{}
-
-		// selection of cells (td) in the row
-		row = row.Find("td")
-		// first cell in the row, span, text (might have issue with multiple a tags)
-		parsed_row.row_num = row.First().Find("span").Text()
-		// second cell in the row - span - second a - text
-		parsed_row.award_num = row.Next().Find("span").Find("a").Next().Text()
-		// third cell in the row - span - second a - text
-		parsed_row.delv_order_num = row.Next().Find("span").Find("a").Next().Text()
-		// fourth cell in the row - span - text
-		parsed_row.delv_order_cnt = row.Next().Find("span").Text()
-		// fifth cell in the row - span - text
-		parsed_row.last_mod_posting_date = row.Next().Find("span").Text()
-		// sixth cell in the row - span - a - text
-		parsed_row.a_cage_code = row.Next().Find("span").Find("a").Text()
-		// seventh cell in the row - span - text
-		parsed_row.total_contract_price = row.Next().Find("span").Text()
-		// eighth cell in the row - span - text
-		parsed_row.award_date = row.Next().Find("span").Text()
-		// ninth cell in the row - span - text
-		parsed_row.posted_date = row.Next().Find("span").Text()
-		// tenth cell in the row - span - text
-		parsed_row.nsn_part_number = row.Next().Find("span").Text()
-		// eleventh cell in the row - span - text
-		parsed_row.nomenclature = row.Next().Find("span").Text()
-		// twelfth cell in the row - span - text
-		parsed_row.purchase_request_number = row.Next().Find("span").Text()
-		// thirteenth cell in the row - span - text
-		parsed_row.solicitation_number = row.Next().Find("span").Text()
-
-		// store the scraped row
-		Awards_Page = append(Awards_Page, parsed_row)
-	})
-
-	finder := "tbody"
-	fmt.Println("Finder: ", finder)
-
-	sel := doc.Children()
-	for i := range sel.Nodes {
-		single := sel.Eq(i)
-		h, err := single.Html()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("Node ", i, " text: ", h[0:5])
-	}
-
-	//fmt.Println("first 100: ", html[:100])
-
-	//fmt.Println("Awards Page object: ", Awards_Page)
-
-	return Awards_Page
 }
